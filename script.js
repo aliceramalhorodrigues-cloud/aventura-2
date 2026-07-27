92c2a02ed23a9301c926f7a294a18926784fc6a1
aliceramalhorodrigues-cloud <alice.ramalho.rodrigues@escola.pr.gov.br>
2026-07-27 11:51:55 -0300

    1

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const avança = document.querySelectorAll('.btn-proximo');
+const avanca = document.querySelectorAll('.btn-proximo');
 
 avanca.forEach(button => {
-    button.addEventListener('click', funtion(){
+    button.addEventListener('click', function(){
         const atual = document.querySelector('.ativo');
         const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+       
         atual.classList.remove('ativo');
         document.getElementById(proximoPasso).classList.add('ativo');
 
